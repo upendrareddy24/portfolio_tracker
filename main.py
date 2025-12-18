1abd64067260115a037ce17be6ff3aae2d237773
Bhavana5N <34437924+Bhavana5N@users.noreply.github.com>
2025-12-17 23:24:14 -0500

    Fix CSS loading: Update Flask static routing

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = Flask(__name__, static_url_path='')
 def serve_index():
     return send_from_directory('.', 'index.html')
 
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory('.', path)
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
